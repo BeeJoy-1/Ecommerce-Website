c3588126af7188e351c06e58d900c88082f48fd9
BeeJoy-1 <beejoy3000@gmail.com>
2023-09-20 09:20:36 +0600

    all done

--- Script.js
+++ Script.js
@@ -1,3 +1,4 @@
+
 const bar = document.getElementById("bar");
 const nav = document.getElementById("navbar")
 const close = document.getElementById("close")
@@ -11,4 +12,20 @@ if (close) {
     close.addEventListener("click", () => {
         nav.classList.remove("active");
     })
+}
+
+var MainImg = document.getElementById("MainImg")
+var smallimg = document.getElementsByClassName("small-img")
+
+smallimg[0].onclick = function (){
+    MainImg.src = smallimg[0].src;
+}
+smallimg[1].onclick = function (){
+    MainImg.src = smallimg[1].src;
+}
+smallimg[2].onclick = function (){
+    MainImg.src = smallimg[2].src;
+}
+smallimg[3].onclick = function (){
+    MainImg.src = smallimg[3].src;
 }
